b1dc318c233b66c674d19a4e839022387103e011
Mobas <gadiner1@gmail.com>
2024-01-04 15:55:03 +0300

    Adding choice of password strength

--- main.py
+++ main.py
@@ -15,15 +15,25 @@ class PasswordGenerator(QWidget):
         button.clicked.connect(self.generate)
         self.output_label = QLabel("")
 
-        grid.addWidget(button, 0, 0)
-        grid.addWidget(self.output_label, 1, 0)
+        self.choice_label = QLabel("Choose your password strength in length")
+
+        self.combo = QComboBox()
+        self.pass_range = list(range(24))
+        self.pass_range = [str(digit) for digit in self.pass_range]
+        self.combo.addItems(self.pass_range)
+
+        grid.addWidget(self.choice_label, 0, 0)
+        grid.addWidget(self.combo, 0, 1)
+        grid.addWidget(button, 2, 0, 1, 2)
+        grid.addWidget(self.output_label, 3, 0)
 
         self.setLayout(grid)
 
     def generate(self):
         SYMBOLS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+'
         password = []
-        for _ in range(12):
+        password_gen_range = int(self.combo.currentText())
+        for _ in range(password_gen_range):
             random_number = random.randint(0, b=len(SYMBOLS)-1)
             letter = SYMBOLS[random_number]
             if letter.isalpha():
